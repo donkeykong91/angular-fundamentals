0c350c987e4c74517e41cea3d0532b1db28de777
donkeykong91 <darylnator@gmail.com>
2020-04-16 21:41:03 -0700

    added interpolation in the template

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,23 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-root',
     styleUrls: ['app.component.scss'],
-    template: ``
+    template: `
+        <div class="app">
+            {{title + '!'}}
+            <div>
+                {{numberOne + numberTwo}}
+            </div>
+            <div>
+                {{isHappy ? ':)' : ':('}}
+            </div>
+        </div>
+    `
 })
 export class AppComponent {
     title: string;
+    numberOne: number = 1;
+    numberTwo: number = 2;
+    isHappy: boolean = true;
     constructor() {
         this.title = 'ultimate angular';
     }
