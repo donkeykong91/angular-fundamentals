ea03fe54da23ff5b4845b9bb40be67549f9add45
donkeykong91 <darylnator@gmail.com>
2020-04-19 05:04:57 -0700

    added new presentation components

---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'passenger-detail',
+    template: `
+        <div>
+            detail component
+        </div>
+    `
+})
+export class PassengerDetailComponent {
+    constructor() {}
+}
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,10 +1,18 @@
 import { NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+
+//smart component
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
+//dumb component
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
 @NgModule({
     declarations: [
-        PassengerDashboardComponent
+        PassengerDashboardComponent,
+        PassengerCountComponent,
+        PassengerDetailComponent,
     ],
     imports: [
         CommonModule
