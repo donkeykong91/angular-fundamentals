96a1998bac57dcab406faa28e4b3dae8a11bd80f
donkeykong91 <darylnator@gmail.com>
2020-04-27 05:47:49 -0700

    added route module and replaced component directive with rotuer-outlet directive

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,26 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
     PassengerDashboardModule,
+    RouterModule.forRoot(routes),
   ],
   bootstrap: [AppComponent]
 })
