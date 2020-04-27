bf25271808b08bf14b0129aa1bee3bc9103b7404
donkeykong91 <darylnator@gmail.com>
2020-04-27 05:52:34 -0700

    added wildcard routing for handling pages that don't exist

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,15 +6,18 @@ import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
---
+++ app/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'not-found',
+    template: `
+        <div>
+            not found
+        </div>
+    
+    `
+})
+export class NotFoundComponent {}
