dea5344cf797458ceb8d41ee7b8485c1c8ab229f
donkeykong91 <darylnator@gmail.com>
2020-04-26 23:20:50 -0700

    added router moudule into app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
