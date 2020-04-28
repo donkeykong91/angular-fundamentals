341f3a520cf12f1fec308e751b6aebc6745b64c1
donkeykong91 <darylnator@gmail.com>
2020-04-28 05:16:33 -0700

    added new dynamic route with param in the route

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -17,7 +17,11 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 import { PassengerDashboardService } from './passenger-dashboard.service';
 
 const routes: Routes = [
-    {path: 'passengers', component: PassengerDashboardComponent}
+    {path: 'passengers',
+        children: [
+            {path: '', component: PassengerDashboardComponent },
+            {path: ':id', component: PassengerViewerComponent }
+        ]}
 ]
 
 @NgModule({
